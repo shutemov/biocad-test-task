22b5759d7867f4842cb88bda5fb46a93e6023b3c
shutemov <shutemov1997@mail.ru>
2020-04-04 15:19:24 +0300

    Update main.js
    Registered vuex and bulma

--- instrument-calibration-report-page/src/main.js
+++ instrument-calibration-report-page/src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import bulma from 'bulma'
+import vuex from 'vuex'
+import 'bulma/css/bulma.css'
+
+Vue.use(bulma)
+Vue.use(vuex)
+
+
 
 Vue.config.productionTip = false
 
