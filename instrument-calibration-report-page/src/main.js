754b4accea2eda954f9d680dd9668e4acd3398f8
shutemov <shutemov1997@mail.ru>
2020-04-04 19:27:55 +0300

    Update main.js
    Added bulma-checkradio to Vue

--- instrument-calibration-report-page/src/main.js
+++ instrument-calibration-report-page/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import bulma from 'bulma'
+import bulmaCheckradio from 'bulma-checkradio'
 import vuex from 'vuex'
 import 'bulma/css/bulma.css'
 
 Vue.use(bulma)
+Vue.use(bulmaCheckradio)
 Vue.use(vuex)
 
 
