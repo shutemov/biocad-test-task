7dcf249dc87e0da9faf2f788d5f19d552dce04e2
shutemov <shutemov1997@mail.ru>
2020-04-04 22:42:17 +0300

    Create DeviceInfo.vue

---
+++ instrument-calibration-report-page/src/components/DeviceInfo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="notification" id="body-block-device-info">
+        <div v-for="(item,index) in 12" :key="'item_'+index">{{index}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DeviceInfo",
+
+        data(){
+            return{
+          
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #body-block-device-info {
+        border: 1px solid;
+        margin-left: 30px;
+        width: 550px;
+        height: 330px;
+    }
+</style>
