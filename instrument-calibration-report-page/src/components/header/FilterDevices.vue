d5f5e27087e8fa3807c0e8d207719a4602ee62e7
shutemov <shutemov1997@mail.ru>
2020-04-05 19:34:43 +0300

    Moving components
    Сomponents FilterDevices moved to "header" directory

---
+++ instrument-calibration-report-page/src/components/header/FilterDevices.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="filter-devices">
+        <div style="text-align: start;">
+            <span id="filter-devices-title">Enter inventory number, guid or bims id</span>
+        </div>
+
+        <div class="field" id="filter-devices-input">
+            <div class="control">
+                <input class="input is-small" type="text" placeholder="Normal input">
+            </div>
+            <button class="button is-dark">Search</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FilterDevices"
+    }
+</script>
+
+<style scoped>
+    #filter-devices {
+        border: 1px solid;
+        width: 320px;
+        height: 90px;
+        /*margin-top: 30px;*/
+        /*margin-left: 50px;*/
+    }
+
+    #filter-devices-title {
+        height: 24px;
+
+        /* Subheading Mobile */
+
+
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 24px;
+
+        /* identical to box height, or 150% */
+
+        letter-spacing: 0.16px;
+        font-feature-settings: 'pnum' on, 'lnum' on;
+
+        /* 8C8C8C */
+
+        color: #8C8C8C;
+    }
+
+
+    #filter-devices button {
+        width: 90px;
+        height: 40px;
+        border-radius: 0
+    }
+
+    #filter-devices-input {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 16px;
+    }
+
+    #filter-devices-input input {
+        width: 210px;
+        height: 30px;
+    }
+</style>
