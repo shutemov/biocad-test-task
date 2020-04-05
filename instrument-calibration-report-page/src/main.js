9b2b6e8505e2fd1dd48f93b8bd4c8bb593b4c656
shutemov <shutemov1997@mail.ru>
2020-04-05 21:54:21 +0300

    Update main.js
    Registered bulma-tooltip

--- instrument-calibration-report-page/src/main.js
+++ instrument-calibration-report-page/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import bulma from 'bulma'
 import bulmaCheckradio from 'bulma-checkradio'
+import bulmaTooltip from 'bulma-tooltip'
 import vuex from 'vuex'
 import 'bulma/css/bulma.css'
 
 Vue.use(bulma)
 Vue.use(bulmaCheckradio)
+Vue.use(bulmaTooltip)
 Vue.use(vuex)
 
 
