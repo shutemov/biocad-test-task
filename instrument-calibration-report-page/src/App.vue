475ae42bf12768ca55e44a48385416d506c4ca06
shutemov <shutemov1997@mail.ru>
2020-04-07 19:43:49 +0300

    Switching layout from bulma to naked css
    Added global padding and the main component is centered.

--- instrument-calibration-report-page/src/App.vue
+++ instrument-calibration-report-page/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="section" id="app">
+  <div id="app">
     <insrument-calibration-report></insrument-calibration-report>
   </div>
 </template>
@@ -17,6 +17,12 @@ export default {
 
 <style>
 #app {
+  /*form*/
+  display: flex;
+  justify-content: center;
+  padding: 30px 50px 100px 50px;
+
+  /*font*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-family: "TT Norms Regular";
