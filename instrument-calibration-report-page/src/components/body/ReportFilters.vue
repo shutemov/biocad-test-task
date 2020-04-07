e6466ab2cc5f5e592390a63c7f47832b2d191876
shutemov <shutemov1997@mail.ru>
2020-04-07 10:22:24 +0300

    Implemented reporting type selection

--- instrument-calibration-report-page/src/components/body/ReportFilters.vue
+++ instrument-calibration-report-page/src/components/body/ReportFilters.vue
@@ -11,39 +11,73 @@
 
 
         <div class="field" style="display: flex;flex-direction: column;margin-top: 10px;">
+
             <div class="field">
-                <input class="is-checkradio is-info" id="exampleRadioNormal" type="radio"
+                <input class="is-checkradio is-info"
+                       id="input1"
+                       type="radio"
                        name="exampleRadioNormal"
-                       checked="checked">
-                <label for="exampleRadioNormal">Calibration</label>
-            </div>
-            <div class="field">
-                <input class="is-checkradio is-info" id="exampleRadioNormal1" type="radio"
-                       name="exampleRadioNormal"
-                       checked="checked">
-                <label for="exampleRadioNormal1">Measuring</label>
+                       value="Calibration"
+                       checked="checked"
+                       v-model="reportType"
+                       @change="setReportType">
+                <label for="input1">Calibration</label>
             </div>
 
             <div class="field">
-                <input class="is-checkradio is-info" id="exampleRadioNormal2" type="radio"
+                <input class="is-checkradio is-info"
+                       id="input2"
+                       type="radio"
                        name="exampleRadioNormal"
-                       checked="checked">
-                <label for="exampleRadioNormal2">Using</label>
+                       value="Measuring"
+                       v-model="reportType"
+                       @change="setReportType">
+                <label for="input2">Measuring</label>
+            </div>
+
+            <div class="field">
+                <input class="is-checkradio is-info"
+                       id="input3"
+                       type="radio"
+                       name="exampleRadioNormal"
+                       value="Using"
+                       v-model="reportType"
+                       @change="setReportType">
+                <label for="input3">Using</label>
             </div>
         </div>
-
     </div>
 
 </template>
 
 <script>
+    import {SET_REPORT_TYPE} from "@/store/mutations";
+    import {mapMutations} from "vuex";
+
     export default {
-        name: "ReportFilters"
+        name: "ReportFilters",
+
+
+        data() {
+            return {
+                reportType: undefined,
+            }
+        },
+
+
+        methods: {
+            ...mapMutations([SET_REPORT_TYPE]),
+
+            setReportType() {
+                console.log(this.reportType)
+                this[SET_REPORT_TYPE](this.reportType)
+            }
+        }
     }
 </script>
 
 <style scoped>
-    #report-filter{
+    #report-filter {
         margin-top: 30px;
     }
 </style>
